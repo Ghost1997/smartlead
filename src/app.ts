5945fd81ca13fe518bd86e3311d1a7e816dbb0c6
Sujit Kumar <mailatsujit@gmail.com>
2023-08-23 00:24:46 +0530

    second Commit

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/api", emailRoutes);
-
+app.get("/healthcheck", (req, res) => {
+  res.status(200).send("Server is up and running");
+});
 sequelize.sync().then(() => {
   console.log("DB Connected");
   app.listen(PORT, () => {
